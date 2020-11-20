e8311251e0fbad76cf563bf6d76657e24b5f6461
NJKongelf <56114977+NJKongelf@users.noreply.github.com>
2020-11-20 12:34:51 +0100

    SubjectRest updated

--- src/main/java/se/iths/rest/SubjectRest.java
+++ src/main/java/se/iths/rest/SubjectRest.java
@@ -1,12 +1,30 @@
 package se.iths.rest;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import se.iths.entity.Subject;
+import se.iths.service.SubjectService;
 
-/*@Path("subject")
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("subject")
 @Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)*/
+@Produces(MediaType.APPLICATION_JSON)
 public class SubjectRest {
+    @Inject
+    SubjectService subjectService;
+
+    @Path("new")
+    @POST
+    public Response createSubject(Subject subject){
+      return Response.ok(subjectService.createSubject(subject)).build();
+    }
+
+    @Path("getall")
+    @GET
+    public List<Subject> getallSubjects(){
+        return subjectService.getAllSubjects();
+    }
 }
